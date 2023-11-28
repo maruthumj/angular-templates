b75b8cc65430a9bdf0b83d520abf7966715d1b07
maruthumj <maruthupandian72@gmail.com>
2023-11-28 21:26:15 +0530

    created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomePageComponent } from './e-commerce/home-page/home-page.component';
+import { ProfilePageComponent } from './e-commerce/profile-page/profile-page.component';
+import { CheckoutComponent } from './e-commerce/checkout/checkout.component';
+import { OrdersComponent } from './e-commerce/orders/orders.component';
+import { MenubarModule } from 'primeng/menubar';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    ProfilePageComponent,
+    CheckoutComponent,
+    OrdersComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MenubarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
